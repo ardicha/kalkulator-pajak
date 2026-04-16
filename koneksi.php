e20858dadaff5d6525f39c9abf24f1c3b9c0d6d0
ardicha <ardichatd@gmail.com>
2026-04-16 22:29:09 +0700

    connection to the database

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,9 @@
 <?php
 
-$host     = "teguhprasetyo.web.id";
-$user     = "dicha";
-$password = "2aUQ28Rw";
-$database = "pajak";
-$port     = 3306;
+$conn = mysqli_connect("localhost","root","","db_pajak");
 
-$conn = mysqli_connect($host, $user, $password, $database, $port);
-
-if (!$conn) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+if(!$conn){
+    die("Koneksi database gagal");
 }
+
 ?>
